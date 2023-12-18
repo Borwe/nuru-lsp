c4d318ae28863befa21d6b1e22c76d914e4a302f
brian.orwe <brian.orwe@gmail.com>
2023-12-18 15:14:54 +0300

    Added notify method

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.20.1
 require github.com/AvicennaJr/Nuru v0.5.16
 
 require (
-	github.com/Borwe/go-lsp v0.0.0-20231218101043-53d22869c872
+	github.com/Borwe/go-lsp v0.0.0-20231218120627-c3edcd9b2723
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
--- server/server.go
+++ server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"github.com/Borwe/go-lsp/jsonrpc"
+	"github.com/Borwe/go-lsp/logs"
 	"github.com/Borwe/go-lsp/lsp"
 	"github.com/Borwe/go-lsp/lsp/defines"
 )
@@ -10,3 +12,14 @@ var Server = lsp.NewServer(&lsp.Options{
 		TriggerCharacters: &[]string{"."},
 	},
 })
+
+// Send notifications to the client
+func Notify(s *lsp.Server, msg jsonrpc.ResponseMessage) {
+	session := s.RpcServer.Session[0]
+	err := session.Write(msg)
+	if err!=nil {
+		logs.Printf("Error occured onNotify: %s\n",err)
+	}else{
+		logs.Printf("Notified %s", msg)
+	}
+}
