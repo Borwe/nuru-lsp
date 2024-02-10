5d52b1d85a99a3b2a122c07954b63afc4d55391e
brian.orwe <brian.orwe@gmail.com>
2024-02-10 21:28:42 +0300

    Add Tree Building Tests

---
+++ tests/treebuilding_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import "testing"
+
+func TestBuildingTree1(t *testing.T) {
+	t.Fatalf("NOT IMPLEMENTED YET")
+}
+
+func TestBuildingTree2(t *testing.T) {
+	t.Fatalf("NOT IMPLEMENTED YET")
+}
