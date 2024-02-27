4ef1b16d5e3388dcedd92b7644b9b365da568264
brian.orwe <brian.orwe@gmail.com>
2024-02-28 00:52:16 +0300

    Bindings now work

--- nuru-tree-sitter/binding_test.go
+++ nuru-tree-sitter/binding_test.go
@@ -1,6 +1,9 @@
 package nuru_tree_sitter
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"testing"
 
 	sitter "github.com/smacker/go-tree-sitter"
@@ -9,4 +12,23 @@ import (
 func TestSimpleProgramIsParsed(t *testing.T) {
 	parser := sitter.NewParser()
 	parser.SetLanguage(GetLanguage())
+	file, err := os.ReadFile("../a.nr")
+	if err != nil {
+		t.Fatalf("Gotten error opening file: %s", err)
+	}
+
+	tree, err := parser.ParseCtx(context.Background(), nil, file)
+	if err != nil {
+		t.Fatalf("Error reading tree: %s", err)
+	}
+
+	firstChildFunction := tree.RootNode().ChildByFieldName("functionname")
+	if firstChildFunction == nil {
+		t.Fatal("First function not found")
+	}
+
+	starts := firstChildFunction.StartByte()
+	ends := firstChildFunction.EndByte()
+
+	fmt.Printf("FIrst function variable Name is: %s", file[starts:ends])
 }
