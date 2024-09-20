d34679752e9080b4e8423afe5df8ce179bdbda47
brian.orwe <brian.orwe@gmail.com>
2024-09-20 14:40:13 +0300

    Added testing

---
+++ vscode-plugin/.vscode-test.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from '@vscode/test-cli';
+import * as dotenv from "dotenv"
+
+function getInstallationConfig(){
+	dotenv.config()
+	const path = process.env.VSCODE_PATH
+	if(path){
+		return {
+			fromPath: path
+		}
+	}
+	return undefined
+}
+
+export default defineConfig({
+	files: 'out/test/**/*.test.js',
+	useInstallation: getInstallationConfig()
+});
