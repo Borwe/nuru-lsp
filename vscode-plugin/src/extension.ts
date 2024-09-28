9ce07228f46429f322912297e6cc0b138361f3e9
brian.orwe <brian.orwe@gmail.com>
2024-09-28 15:03:40 +0300

    Downloading tests now work

--- vscode-plugin/src/extension.ts
+++ vscode-plugin/src/extension.ts
@@ -16,54 +16,31 @@ let client: LanguageClient;
 /** Hold information on location of lsp file to execute */
 let command = "nuru-lsp"
 
-const LINK_BASE = `https://github.com/Borwe/nuru-lsp/releases/download/v${VERSION}/`
-
-async function downloadLSPExecutable(link: string) {
-}
-
 export function activate(context: ExtensionContext) {
   Context = context
   //register commands
   commands.registerCommand("nuru.languageserver.is-installed", isInstalled);
   commands.registerCommand("nuru.languageserver.download", downloadOrUpdate);
-  commands.registerCommand("nuru.languageserver.restart", async ()=>{
-    if(client.isRunning()){
+  commands.registerCommand("nuru.languageserver.restart", async () => {
+    if (client.isRunning()) {
       await client.stop()
     }
     client.start()
     window.showInformationMessage("Nuru LSP restarted")
   });
-  commands.registerCommand("nuru.languageserver.start", async ()=>{
-    if(!client.isRunning()){
+  commands.registerCommand("nuru.languageserver.start", async () => {
+    if (!client.isRunning()) {
       await client.start()
     }
     window.showInformationMessage("Nuru LSP started")
   });
-  commands.registerCommand("nuru.languageserver.stop", async ()=>{
-    if(client.isRunning()){
+  commands.registerCommand("nuru.languageserver.stop", async () => {
+    if (client.isRunning()) {
       await client.stop()
     }
     window.showInformationMessage("Nuru LSP stopped")
   });
 
-  (async ()=>{
-    let cmd: string = "nuru-lsp"
-    let url = LINK_BASE+"nuru-lsp-ubuntu-latest.zip"
-    let result: Promise<string> 
-    if(os.platform() === "win32"){
-      url = LINK_BASE + "nuru-lsp-windows-latest.zip"
-      cmd = "nuru-lsp.exe"
-    }
-    if(os.platform() === "darwin"){
-      url = LINK_BASE + "nuru-lsp-macos-latest.zip"
-    }
-    exec(cmd, async (err: ExecException|null, stdout: string, stderr: string)=>{
-      if(err!=null){
-        await downloadLSPExecutable(url)
-      }
-    })
-    return result
-  })()
 
   const serverOptions: ServerOptions = {
     run: { command: command, transport: TransportKind.stdio },
@@ -73,7 +50,7 @@ export function activate(context: ExtensionContext) {
   // Options to control the language client
   const clientOptions: LanguageClientOptions = {
     // Register the server for all documents by default
-    documentSelector: [{ language: "nuru" }],
+    documentSelector: [{ language: "nuru", pattern: "*.{nr,sr}" }],
     synchronize: {
       // Notify the server about file changes to '.clientrc files contained in the workspace
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
@@ -89,9 +66,9 @@ export function activate(context: ExtensionContext) {
   );
 
   // Start the client. This will also launch the server
-  client.start().catch((err)=>{
-    window.showErrorMessage(`Nuru Lsp failed to start error: ${err}`)
-  });
+  //client.start().catch((err) => {
+  //  window.showErrorMessage(`Nuru Lsp failed to start error: ${err}`)
+  //});
 }
 
 export function deactivate(): Thenable<void> | undefined {
--- vscode-plugin/src/test/extension.test.ts
+++ vscode-plugin/src/test/extension.test.ts
@@ -1,16 +1,39 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
-import * as extension from '../extension'
 import { getLatestReleaseVersion } from '../utils';
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
 
 suite('Getting nuru-lsp file', () => {
+
+	//get an vscode.ExtensionContext instance
+	const context = vscode.extensions.getExtension("BrianOrwe.nuru-lsp");
+	//clear the nuru-lsp.zip and nuru-lsp executable
+	const extPath = context.extensionPath.replace(/\\/g, "/")
+	const zip = path.join(extPath, "nuru-lsp.zip").replace(/\\/g, "/")
+	const exe = path.join(extPath,
+		`nuru-lsp${os.platform() === "win32" ? ".exe" : ""}`).replace(/\\/g, "/")
+	console.log("EXE:",exe)
+	if (fs.existsSync(exe)) {
+		fs.unlinkSync(exe)
+	}
+	if (fs.existsSync(zip)) {
+		console.log("ZIP:",zip)
+		fs.unlinkSync(zip)
+	}
+	assert.strictEqual(fs.existsSync(exe), false)
+	assert.strictEqual(fs.existsSync(zip), false)
+
+	console.log("YOLO!!")
+	
 
 	test('Checking if nuru-lsp not in dir', async () => {
 		const exists = await vscode.commands.executeCommand("nuru.languageserver.is-installed")
 		assert.strictEqual(exists, false);
 	});
 
-	test("Test getting latest release", async()=>{
+	test("Test getting latest release", async () => {
 		const num = await getLatestReleaseVersion()
 		assert.strict.notEqual(num, 0)
 	})
@@ -22,3 +45,9 @@ suite('Getting nuru-lsp file', () => {
 		assert.strictEqual(exists, true);
 	});
 });
+
+suite('Testing LSP workings', () => {
+	test("nuru-lSP attatches to nr file one start", () => {
+		assert.strictEqual(false, true, "NOT IMPLEMENTED")
+	})
+})
